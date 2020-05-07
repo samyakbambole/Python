6893b3b933f11cadf77f979501b7aa13265dc0c8
samyakbambole <samyak_study@hotmail.com>
2020-05-07 18:20:44 +0530

    Updates

---
+++ Learning/9.Lists.py
@@ -0,0 +1,9 @@
+# List Program
+#7/5/2020
+
+friends = ["Ayush", "Surabhi", "Samyak"]
+
+print(friends)
+print(friends[2]) # You can use indexes in python lists
+print(friends[-2])
+print(friends[1:])

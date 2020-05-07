bad7b742296fbdf9a4825a79841cfe6f08445942
samyakbambole <samyak_study@hotmail.com>
2020-05-07 13:48:57 +0530

    Updates

---
+++ Learning/8.Mad_Libs_Game.py
@@ -0,0 +1,10 @@
+# Mad Libs Game in Python
+# 7/5/2020 
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", celebrity)

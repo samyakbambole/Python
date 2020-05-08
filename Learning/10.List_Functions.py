8931aafc24b3f20e9e7744b418df1be16804afec
samyakbambole <samyak_study@hotmail.com>
2020-05-08 10:29:49 +0530

    Updates

---
+++ Learning/10.List_Functions.py
@@ -0,0 +1,16 @@
+# List Functions Program
+# 8/5/2020
+
+friends = []
+friends.append("Oscar")
+friends.append("Angela")
+friends.insert(1, "Kevin")
+
+# friends.remove("Kevin")
+print( friends )
+print( friends.index("Oscar") )
+print( friends.count("Angela") )
+friends.sort()
+print( friends )
+friends.clear()
+print( friends )

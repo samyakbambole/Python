9f18204aca7422cd6f3d052c9ce973bf9fd15b1f
samyakbambole <samyak_study@hotmail.com>
2020-05-08 10:49:17 +0530

    Updates

--- Learning/10.List_Functions.py
+++ Learning/10.List_Functions.py
@@ -1,16 +1,30 @@
-# List Functions Program
-# 8/5/2020
+#List functions Program
+#8/5/2020
 
-friends = []
-friends.append("Oscar")
-friends.append("Angela")
-friends.insert(1, "Kevin")
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Samyak", "Ayush", "Surabhi", "Jayden", "Ayush.H", "Laptop" ]
+friends2 = friends.copy()
 
-# friends.remove("Kevin")
-print( friends )
-print( friends.index("Oscar") )
-print( friends.count("Angela") )
-friends.sort()
-print( friends )
-friends.clear()
-print( friends )
+friends.extend(lucky_numbers) # This will extend the elements of the list.
+
+friends.append("Sam") # This will add another item / string to the list
+
+friends.insert(2, "Laptop") # This will insert at the given index position.
+
+friends.remove("Jayden")
+
+# friends.clear() This will clear the whole list
+
+friends.pop() # This will remove the last element
+
+print(friends.index("Laptop"))
+
+print(friends.count("Laptop"))
+
+lucky_numbers.sort() # This will sort the numbers in ascending order
+
+lucky_numbers.reverse()
+
+print(friends)
+
+print(lucky_numbers)

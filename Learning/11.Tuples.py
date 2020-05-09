31ce16b901f65fc67bac7de64e5f0d7ad0528962
samyakbambole <samyak_study@hotmail.com>
2020-05-09 20:45:42 +0530

    Updates

---
+++ Learning/11.Tuples.py
@@ -0,0 +1,15 @@
+#Tuples Program
+#9/5/2020
+
+lucky_numbers = (4, 8, "fifteen", 16, 23, 42.0)
+#      indexes  0  1       2      3   4   5
+
+# lucky_numbers[0] = 90  # tuples are immutable
+print(lucky_numbers[0])
+print(lucky_numbers[1])
+print(lucky_numbers[-1])
+print(lucky_numbers[2:])
+print(lucky_numbers[2:4])
+print(len(lucky_numbers))
+
+c
